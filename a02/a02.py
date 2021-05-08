3d2e7caecb8189b6444ed88c13bc5dd0a6846348
yaragd <yaragd@github.com>
2021-05-09 02:07:19 +0300

    a02
    code_# (1 - 4)

--- a02/a02.py
+++ a02/a02.py
@@ -1,43 +1,45 @@
-"""Provides a scripting component.
-    Inputs:
-        m: a mesh
-        s: sun vector
-    Output:
-        a: List of Vectors
-        b: List of Points
-        c: list of angles
-        d: exploded mesh
-        """
-        
 import Rhino.Geometry as rg
+
 
 #1.
 #compute face normals using rg.Mesh.FaceNormals.ComputeFaceNormals()
+faceNormals = []
+facenum = len(m.Faces)
+for i in range(facenum):
+    norm = m.FaceNormals[i]
+    normNeg = rg.Vector3d.Negate(norm)
+    faceNormals.append(normNeg)
 #output the vectors to a
-
 a = faceNormals
 
-#2.
+#2
 #get the centers of each faces using rg.Mesh.Faces.GetFaceCenter()
-#store the centers into a list called centers 
+centers = []
+for i in range(facenum):
+    face = m.Faces.GetFaceCenter(i)
+    centers.append(face)
 #output that list to b
-
 b = centers
 
 #3.
 #calculate the angle between the sun and each FaceNormal using rg.Vector3d.VectorAngle()
+angleList = []
+for i in range(facenum):
+    angle = rg.Vector3d.VectorAngle(a[i],s)
+    angleList.append(angle)
 #store the angles in a list called angleList and output it to c
-
 c = angleList
 
-
-#4. explode the mesh - convert each face of the mesh into a mesh
+#4.
+#explode the mesh - convert each face of the mesh into a mesh
 #for this, you have to first copy the mesh using rg.Mesh.Duplicate()
 #then iterate through each face of the copy, extract it using rg.Mesh.ExtractFaces
+exploded = []
+mesh2 = len(rg.Mesh.Duplicate(m).Faces)
+mDup = m.Duplicate()
+for i in range(mesh2):
+     meshFace = mDup.Faces.ExtractFaces([0])
+     exploded.append(meshFace)
 #and store the result into a list called exploded in output d
-
 d = exploded
 
-#after here, your task is to apply a transformation to each face of the mesh
-#the transformation should correspond to the angle value that corresponds that face to it... 
-#the result should be a mesh that responds to the sun position... its up to you!
